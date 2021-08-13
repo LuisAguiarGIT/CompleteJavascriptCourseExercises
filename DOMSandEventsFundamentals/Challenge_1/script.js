50ae219cb8476dc0017a4b039e80667aa41c8268
Luís Aguiar <luisaguiar96@hotmail.com>
2021-08-13 22:01:03 +0100

    Challenge for this chapter

---
+++ DOMSandEventsFundamentals/Challenge_1/script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+let score = 20;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
+
+  //No value
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number!";
+    //Player wins
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Number!";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    //Too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost!";
+      document.querySelector(".score").textContent = score;
+    }
+    //Too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent = "Too low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost!";
+      document.querySelector(".score").textContent = 0;
+    }
+  }
+});
+
+// This is the challenge
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = 20;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector("body").style.backgroundColor = "#222";
+});
